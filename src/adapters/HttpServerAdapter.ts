83d150016765eeef2475078c05010ade6c28c3ed
leydev <wesley.waaraujo@gmail.com>
2023-03-10 17:17:57 -0300

    FIX: update pattern methods and remove interface

--- src/adapters/HttpServerAdapter.ts
+++ src/adapters/HttpServerAdapter.ts
@@ -1,22 +1,12 @@
+import { IHttpServer, Method, Handler, Middleware } from "@/IHttpServer";
 import express, { Express } from "express"
 
-type Method = "get" | "post" | "patch" | "put" | "delete"
-type Middleware = (request: Record<string, any>, response: Record<string, any>, callback: () => void) => void
-type Handler = (request: Record<string, any>, response: Record<string, any>) => void
-
-export interface IHttpServer {
-    register(method: Method, path: string, handler: Handler): void
-    register(method: Method, path: string, middlewares: Middleware[], handler: Handler): void
-    register(method: Method, path: string, middlewares: Middleware[] | Handler, handler?: Handler): void
-}
-
 export default class HttpServer implements IHttpServer {
-    private server: Express
+    private server: Express;
 
     constructor(port: number, callback?: () => void) {
         this.server = express();
-
-        this.server.listen(port, callback)
+        this.server.listen(port, callback);
     }
 
     /**
@@ -25,14 +15,11 @@ export default class HttpServer implements IHttpServer {
      * @param path "/resource"
      * @param handler (req, res) => {} 
      */
-    register(method: Method, path: string, handler: Handler): void
-    register(method: Method, path: string, middlewares: Middleware[], handler: Handler): void
-    register(method: Method, path: string, middlewares: Middleware[] | Handler, handler?: Handler): void {
+    register = (method: Method, path: string, middlewares: Middleware[] | Handler, handler?: Handler): void => {
         if (handler) {
             this.server[method](path, middlewares, handler);
             return;
         }
-
         this.server[method](path, middlewares)
     }
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,29 @@
-export default function HelloWorld(part: string): string {
-  return "Hello " + part;
+import DataBase from "./adapters/DataBaseAdapter"
+import HttpServer from "./adapters/HttpServerAdapter"
+import HttpController from "./interfaces/HttpController"
+
+/* interface Employee {
+  id: string
+  name: string
+  email: string
+  phone: string
+  birthday: string
+  createdAt: number
+  deletedAt: number
+  active: boolean
 }
 
+interface Action {
+  name: "SEND_EMAIL" | "SEND_WHATSAPP" | "ENABLE_ON_ACTIVE_DIRECTORY"
+} */
+const database = new DataBase("mongodb://admin:password@localhost:27017")
+
+database.connect().then(() => {
+  console.log("Database connected!")
+})
+
+/* const PORT = Number(process.env.PORT || 3000);
+
+const httpServer = new HttpServer(PORT, () => console.info(`Server on localhost:${PORT}`));
+new HttpController(httpServer); */
 
