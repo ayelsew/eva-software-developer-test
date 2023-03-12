bb95426105ebf92850622507375ac7edb37d90ec
leydev <wesley.waaraujo@gmail.com>
2023-03-12 12:20:18 -0300

    ADD: connect classes

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,17 @@
-import DataBase from "./adapters/DataBaseAdapter"
-import HttpServer from "./adapters/HttpServerAdapter"
-import HttpController from "./interfaces/HttpController"
+import employeeEntity, { EmployeeAttributes } from "./core/domain/entities/employeeEntity";
+import employeeManager from "./core/usecase/employeeManager";
+import dataBaseOnSQLAdapter from "./infrastructure/adapters/DataBaseAdapter";
+import employeeDAO from "./infrastructure/employeeDAO";
+import httpServerAdapter from "./interfaces/adapters/httpServerAdapter";
+import httpController from "./interfaces/httpController";
 
-/* interface Employee {
-  id: string
-  name: string
-  email: string
-  phone: string
-  birthday: string
-  createdAt: number
-  deletedAt: number
-  active: boolean
-}
-
-interface Action {
-  name: "SEND_EMAIL" | "SEND_WHATSAPP" | "ENABLE_ON_ACTIVE_DIRECTORY"
-} */
-const database = new DataBase("mongodb://admin:password@localhost:27017")
-
-database.connect().then(() => {
-  console.log("Database connected!")
+const httpsServer = httpServerAdapter(3000, () => {
+  console.log(`Server on localhost:3000`)
 })
 
-/* const PORT = Number(process.env.PORT || 3000);
+const connectionDB = dataBaseOnSQLAdapter<EmployeeAttributes>("mongodb://admin:password@localhost:27017")
+const employeeDB = employeeDAO(connectionDB)
+const manager = employeeManager(employeeDB, employeeEntity)
 
-const httpServer = new HttpServer(PORT, () => console.info(`Server on localhost:${PORT}`));
-new HttpController(httpServer); */
+httpController(httpsServer, manager)
 
