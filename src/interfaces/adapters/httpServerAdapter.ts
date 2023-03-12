6152111b778a8a9dc8f1162510ad615f797e6381
leydev <wesley.waaraujo@gmail.com>
2023-03-12 12:19:30 -0300

    FIX: update server http adatper

---
+++ src/interfaces/adapters/httpServerAdapter.ts
@@ -0,0 +1,26 @@
+import { HttpServer, Method, Handler, Middleware } from "@/core/ports/HttpServer";
+import express from "express"
+
+export default function httpServerAdapter(port: number, callback?: () => void): HttpServer {
+  const server = express()
+  server.use(express.json())
+  server.use(express.urlencoded({ extended: true }))
+
+  server.listen(port, callback);
+
+  /**
+   * 
+   * @param method get, post...
+   * @param path "/resource"
+   * @param handler (req, res) => {} 
+   */
+  const register = (method: Method, path: string, middlewares: Middleware[] | Handler, handler?: Handler): void => {
+    if (handler) {
+      server[method](path, middlewares, handler);
+      return;
+    }
+    server[method](path, middlewares)
+  }
+
+  return { register }
+}
---
+++ src/interfaces/httpController.ts
@@ -0,0 +1,26 @@
+import { HttpServer } from "@/core/ports/HttpServer";
+import { EmployeeManagerReturn as EmployeeManager } from "@/core/usecase/employeeManager";
+
+export default function httpController(httpServer: HttpServer, employeeManager: EmployeeManager) {
+
+    httpServer.register("post", "/employee", async (request, response) => {
+        const { status, payload } = await employeeManager.insert(request.body);
+        response.status(status ?? 200).send(payload)
+    })
+
+    httpServer.register("get", "/employee/:id?", async (request, response) => {
+        const { status, payload } = await employeeManager.find(request.params.id);
+        response.status(status ?? 200).send(payload)
+    })
+
+    httpServer.register("patch", "/employee/:id", async (request, response) => {
+        const { status, payload } = await employeeManager.update(request.params.id, request.body);
+        response.status(status ?? 200).send(payload)
+    })
+
+    httpServer.register("delete", "/employee/:id", async (request, response) => {
+        const { status, payload } = await employeeManager.remove(request.params.id);
+        response.status(status ?? 200).send(payload)
+    })
+
+}
